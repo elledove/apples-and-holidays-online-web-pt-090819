14104993fa2401d0396d2b7c431c181e8c48ca96
Lauren Dove <elledoveinc@gmail.com>
2019-09-23 01:54:34 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,6 +71,9 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 bbq_days = []
+holiday_hash.each do |season , holiday|
+  holiday
+end
 end
 
 
