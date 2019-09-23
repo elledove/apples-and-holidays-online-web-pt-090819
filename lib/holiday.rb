76ee59d1da2b895277b1aa28e756c15e22365cf6
Lauren Dove <elledoveinc@gmail.com>
2019-09-23 01:34:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,8 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, holiday|
   puts "#{season.to_s.capitalize}:"
   holiday.each do |day , supplies|
-end
+   end
+ end
 end
 
 def all_holidays_with_bbq(holiday_hash)
