5841f0ee451255aba17c2873406ba91e894c25d7
Lauren Dove <elledoveinc@gmail.com>
2019-09-23 00:44:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,6 +63,31 @@ def all_holidays_with_bbq(holiday_hash)
 end
 
 
+def second_supply_for_fourth_of_july(holiday_hash)
+  # given that holiday_hash looks like this:
+  # {
+  #   :winter => {
+  #     :christmas => ["Lights", "Wreath"],
+  #     :new_years => ["Party Hats"]
+  #   },
+  #   :summer => {
+  #     :fourth_of_july => ["Fireworks", "BBQ"]
+  #   },
+  #   :fall => {
+  #     :thanksgiving => ["Turkey"]
+  #   },
+  #   :spring => {
+  #     :memorial_day => ["BBQ"]
+  #   }
+  # }
+  # return the second element in the 4th of July array
+
+holiday_hash[:summer][:fourth_of_july][1]
+
+
+end
+
+
 
 
 
