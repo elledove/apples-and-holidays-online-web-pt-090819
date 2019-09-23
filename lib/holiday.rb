dd5f75114697619c07dcf94467a57c8b198bea81
Lauren Dove <elledoveinc@gmail.com>
2019-09-23 01:20:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -39,7 +39,7 @@ end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
    holiday_hash[season][holiday_name] = supply_array  
-   #holiday_hash
+   
    # code here
                   # remember to return the updated hash
 
@@ -47,7 +47,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+holiday_hash.values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
